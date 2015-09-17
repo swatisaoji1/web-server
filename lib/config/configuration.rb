8b2ab66ae2bc25f4b107c96b929c46a0f4478197
HariniParth <parth.harini@gmail.com>
2015-09-16 18:19:07 -0700

    Made changes to the configuration file to store the httpd_conf keys-values in a hashmap [Style 2]

--- lib/config/configuration.rb
+++ lib/config/configuration.rb
@@ -3,7 +3,40 @@
 # HttpdConf and MimeTypes, both derived from this parent class.
 module WebServer
   class Configuration
+	attr_accessor :res
+
     def initialize(file_content)
+      #read config files here
+      @config_map = Hash.new
+      file_content.each_line do |line|
+	@config2_map = Hash.new
+        line = line.gsub(/#(.*)/, '')
+        words = line.scan(/\S+/)
+        if(!words.empty?)
+          words.map { |word|   word.gsub!(/\A"|"\Z/, '') }
+          @count = words.length
+	  w1 = words.shift
+ 	  w2 = words.shift
+	  if (@count>2)
+	   @config2_map[w2]=words
+	   res=words
+	  else
+	   @config2_map[w1]=w2
+	   res=w2
+	  end
+	  #puts @config2_map.to_s
+	  if(@config_map[w1].nil?)
+	   @config_map[w1]=@config2_map
+	  else
+	   @config_map[w1].merge!(@config2_map)
+	  end
+        end
+	puts res.to_s	
+      end
+      #puts res.to_s
     end
+
   end
+
 end
+
--- lib/config/httpd_conf.rb
+++ lib/config/httpd_conf.rb
@@ -4,46 +4,87 @@ require_relative 'configuration'
 module WebServer
   class HttpdConf < Configuration
     def initialize(httpd_file_content)
+	super(httpd_file_content)
     end
 
     # Returns the value of the ServerRoot
     def server_root 
+	if @config_map.has_key?("ServerRoot")
+	 @config_map["ServerRoot"].each do |k,v|
+	  return "#{v}"
+  	 end
+ 	end
     end
 
     # Returns the value of the DocumentRoot
     def document_root
+	if @config_map.has_key?("DocumentRoot")
+	 @config_map["DocumentRoot"].each do |k,v|
+          return "#{v}"
+         end
+	end
     end
 
     # Returns the directory index file
     def directory_index
+	if @config_map.has_key?("DirectoryIndex")
+	 @config_map["DirectoryIndex"].each do |k,v|
+          return "#{v}"
+         end
+	end
     end
 
     # Returns the *integer* value of Listen
     def port
+	if @config_map.has_key?("Listen")
+	 @config_map["Listen"].each do |k,v|
+          return "#{v}".to_i
+         end
+	end
     end
 
     # Returns the value of LogFile
     def log_file
+	if @config_map.has_key?("LogFile")
+	 @config_map["LogFile"].each do |k,v|
+          return "#{v}"
+         end
+	end
     end
 
     # Returns the name of the AccessFile 
     def access_file_name
+	if @config_map.has_key?("AccessFile")
+         @config_map["AccessFile"].each do |k,v|
+          return "#{v}"
+         end
+        end
     end
 
     # Returns an array of ScriptAlias directories
     def script_aliases
+	@config_map["ScriptAlias"].each do |k,v|
+		return "#{v}"
+	end
     end
 
     # Returns the aliased path for a given ScriptAlias directory
     def script_alias_path(path)
+	
     end
 
     # Returns an array of Alias directories
     def aliases
-    end
+      	if @config_map.has_key?("Alias")
+      	 @config_map["Alias"].each do |k,v|
+	  puts "#{v}"
+  	 end
+	end
+   end
 
     # Returns the aliased path for a given Alias directory
     def alias_path(path)
+		
     end
-  end
+ end
 end
