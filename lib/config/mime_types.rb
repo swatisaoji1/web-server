bd4cddd7f49faa36b2478ca18fe3e977d14d96e8
HariniParth <parth.harini@gmail.com>
2015-09-18 11:24:11 -0700

    Made changes to mime file

--- lib/config/mime_types.rb
+++ lib/config/mime_types.rb
@@ -4,6 +4,7 @@ require_relative 'configuration'
 module WebServer
   class MimeTypes < Configuration
     def initialize(mime_file_content)
+	super(mime_file_content)
     end
     
     # Returns the mime type for the specified extension
