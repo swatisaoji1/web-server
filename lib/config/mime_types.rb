547c35f9b38e3cb7b8d33d7c6194a7af190fffc1
HariniParth <parth.harini@gmail.com>
2015-09-18 13:27:32 -0700

    Modified the mime file to read extensions

--- lib/config/mime_types.rb
+++ lib/config/mime_types.rb
@@ -3,12 +3,32 @@ require_relative 'configuration'
 # Parses, stores and exposes the values from the mime.types file
 module WebServer
   class MimeTypes < Configuration
-    def initialize(mime_file_content)
-	super(mime_file_content)
+    def initialize(file_content)
+	@config_map = Hash.new
+        file_content.each_line do |line|
+    
+        line = line.gsub(/#(.*)/, '')
+        words = line.scan(/\S+/)
+        if(!words.empty?)
+          words.map { |word|   word.gsub!(/\A"|"\Z/, '') }
+          @count = words.length
+      	  w1 = words.shift
+      	  if(@count>1)
+       	   words.each do |w|
+            @config_map[w]=w1
+           end
+          end
+        end
+        end
     end
     
     # Returns the mime type for the specified extension
     def for_extension(extension)
+	if (@config_map.has_key?(extension))
+	 return @config_map[extension]
+	else
+	 return @config_map["txt"]
+	end
     end
 
   end
